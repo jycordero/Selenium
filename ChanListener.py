b5ec188b88c77fed112298cbbdebe71c1e959076
jcordero <joseph.cordero1@gmail.com>
2019-08-30 17:26:55 -0500

    Added waiting until change on value of Exposure element. Also added email functionality

--- ChanListener.py
+++ ChanListener.py
@@ -1,17 +1,30 @@
 
 # coding: utf-8
 
-# In[5]:
+# In[12]:
 
 
 #!/usr/bin/env python
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+
+# Import smtplib for the actual sending function
+# Import the email modules we'll need
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import email
 
 import time
 
 
-# In[6]:
+# In[13]:
 
 
 #------------ VARIABLES
@@ -24,9 +37,14 @@ fields = [
 IntroValues = {}
 IntroValues["target"] = "GW170817"
 IntroValues["obsidRangeList"]  =  "21372"
+#nextEvt = '22737'
+#IntroValues["obsidRangeList"]  =  nextEvt
 
 
-# In[7]:
+delay = 3
+
+
+# In[14]:
 
 
 #--------- FUNCTIONS
@@ -36,26 +54,93 @@ def FillIntroPage(browser, value):
         elem.send_keys(value[name])               # passes the VALUE to the field
     elem.send_keys(Keys.RETURN)    # Pass the RETURN value
     
-def GetTable(browser,VAR = "exp",ID='21372'):
-    name = VAR+ID
-    XPATH = "//*[@id=\""+name+"\"]"
+def GetXPath(browser,VAR = "exp",ID='21372'):
+    return "//*[@id=\""+VAR+ID+"\"]"
+
+def GetTable(browser,XPATH):
     return browser.find_element_by_xpath(XPATH).text
 
+def ChanListener(webpage,IntroValues,delay):
+    browser = webdriver.Firefox()  # Opens Firefox
+    browser.get(webpage)           # Opens the webpage indicated above
 
-# In[ ]:
+    FillIntroPage(browser, IntroValues) # Fills in the values according to the dictionary 
+
+    time.sleep(1) #Waits for the page to load
+      
+    wait  = WebDriverWait(browser,delay)# Create Wait onbject linked to the browser, checking evey "delay" seconds
+    XPATH = GetXPath(browser,VAR='exp',ID='21372')
+    try:
+        myElem = wait.until(EC.presence_of_element_located((By.XPATH, XPATH)))
+        Status['notReady']  = False
+        Status['Target'] = IntroValues['target']
+        Status['ID']     = IntroValues['obsidRangeList']
+        
+        browser.close()
+        return Status
+    except TimeoutException:
+        Status['notReady']  = True
+        Status['Target'] = None
+        Status['ID']     = None
+        
+        browser.close()
+        return Status
+    
+    
+    
+def SendEmail(Status,
+              mailFrom = 'joseph.cordero1@gmail.com',
+              mailTo   = 'joseph.cordero1@gmail.com',
+             ):
+        
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.login(mailFrom, "PASSWORD")
+    
+    msg = MIMEMultipart()
+    msg['From'] = mailFrom
+    msg['To'] = mailTo
+    if Status['notReady']:
+        msg['Subject'] = 'Chandar NOT ready'
+        msg.attach(MIMEText('Chandra data is NOT ready!\n'+
+                       'Somehow code stopped working\n'+
+                       '\n'+
+                       'You can check status of data here webpage https://cda.harvard.edu/chaser/mainEntry.do \n' 
+                           )
+                  )
+    else:
+        msg['Subject'] = 'Chandar READY!'
+        msg.attach(MIMEText('Chandra data is ready!\n'+
+                       '---Target:: '+Status['Target']+'\n'+
+                       '---ID    :: '+Status['ID']+'\n'+
+                       '\n'+
+                       'Visit webpage https://cda.harvard.edu/chaser/mainEntry.do \n' 
+                           )
+                  )
+
+    server.set_debuglevel(True) # show communication with the server
+    try:
+        server.sendmail(mailFrom, [mailTo], msg.as_string())
+
+    finally:
+        server.quit()
+
+
+# In[15]:
 
 
 #----------- MAIN
 if __name__ == "__main__":#---------
-    browser = webdriver.Firefox()  # Opens Firefox
-    browser.get(webpage)           # Opens the webpage indicated above
+    Status = {'notReady':True}
+    while Status['notReady']:
+        Status = ChanListener(webpage, IntroValues, delay)
+        print("Still waiting for Chandra ¯\_(ツ)_/¯ ")
+        
+    SendEmail(Status)
+        
 
-    FillIntroPage(browser, IntroValues)
 
-    time.sleep(2)
-    
-    tableValue = GetTable(browser)
-    
-    print(tableValue)
-    
+# In[16]:
+
+
+Status
 
